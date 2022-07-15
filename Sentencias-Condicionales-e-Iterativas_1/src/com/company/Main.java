4ccf2651f4086efd345ef516567a7010db0f6b98
lucio albornoz <lucioga32@gmail.com>
2022-07-15 03:26:15 -0300

    metodo ejercicio 6

--- Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
+++ Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Triangulo1();
+    }
+
+    private static void Triangulo1() {
         String caract="*";
         for(int i=0; i<7; i++){
             System.out.println(caract);
