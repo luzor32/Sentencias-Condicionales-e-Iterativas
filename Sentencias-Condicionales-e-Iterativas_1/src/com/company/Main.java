e7cc8d7c1421e38af66774b209ee2d008e85f730
lucio albornoz <lucioga32@gmail.com>
2022-07-15 02:32:02 -0300

    ejercicio 3

--- Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
+++ Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("sentencias condicionales e iterativas");
+        String nombre = "";
+        double nota1 = 0, nota2 = 0, nota3 = 0;
+        double prom_nota = 0;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Ingresa el nombre del estudiante; ");
+        nombre = entrada.nextLine();
+
+        System.out.print("Ingrese la primera nota: ");
+        nota1 = entrada.nextFloat();
+
+        System.out.print("Ingrese la segunda nota: ");
+        nota2 = entrada.nextFloat();
+        System.out.print("Ingrese la tercera nota: ");
+        nota3=entrada.nextFloat();
+
+        prom_nota = (nota1 + nota2 + nota3)/3;
+
+        System.out.println("El estudiante es: " + nombre);
+        System.out.println("Nota promedio: " + prom_nota);
+
+        if (prom_nota>8 && prom_nota<=10){
+            System.out.println("Promocionado");
+        }else if(prom_nota<9 && prom_nota>5){
+            System.out.println("Aprobado");
+        }else if(prom_nota<=5 && prom_nota>0) {
+            System.out.println("Desaprobado");
+        }
     }
 }
