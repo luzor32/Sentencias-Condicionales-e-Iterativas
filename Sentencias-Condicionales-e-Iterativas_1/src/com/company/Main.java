94c0227090700ce51f0f4b47ac90ff1135df0221
lucio albornoz <lucioga32@gmail.com>
2022-07-15 02:46:22 -0300

    ejercicio 8

--- Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
+++ Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("sentencias condicionales e iterativas");
+        String caract="*";
+
+        for(int j=0; j<7; j++) {
+            caract="";
+            for (int i = 0; i < 7-j-1; i++) {
+                caract += " ";
+            }
+            for (int i = 0; i < j+1; i++) {
+                caract += "*";
+            }
+
+            System.out.println(caract);
+        }
     }
 }
