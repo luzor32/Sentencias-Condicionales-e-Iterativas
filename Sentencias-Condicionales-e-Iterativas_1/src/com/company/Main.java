7d03bd5b1317c263e9d4385ff5ff923df5139651
lucio albornoz <lucioga32@gmail.com>
2022-07-15 03:22:53 -0300

    metodo ejercicio 4

--- Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
+++ Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
@@ -3,11 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        MetodoFoo();
+
+    }
+
+    private static void MetodoFoo() {
         String texto="foo";
 
         for(int i=0; i<10; i++){
             System.out.println(texto);
         }
-
     }
 }
