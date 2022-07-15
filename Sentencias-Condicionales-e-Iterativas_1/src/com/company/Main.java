daf54add2725739e0e7e10b57aa9bf1496a95aa0
lucio albornoz <lucioga32@gmail.com>
2022-07-15 02:35:27 -0300

    ejercicio 4

--- Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
+++ Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("sentencias condicionales e iterativas");
+        String texto="foo";
+
+        for(int i=0; i<10; i++){
+            System.out.println(texto);
+        }
+
     }
 }
