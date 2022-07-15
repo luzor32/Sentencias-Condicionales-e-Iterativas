34ecdcae208c982ef00e8a304265b7825b0d983f
lucio albornoz <lucioga32@gmail.com>
2022-07-15 03:24:46 -0300

    metodo ejercicio 5

--- Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
+++ Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Contar();
+    }
+
+    private static void Contar() {
         for(int i=1; i<=10; i++){
             System.out.println("numero " + i);
 
