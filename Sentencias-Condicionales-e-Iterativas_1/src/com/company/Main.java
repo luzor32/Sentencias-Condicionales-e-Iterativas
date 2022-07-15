1adadc366be05521f2b3fe1f79acaf908307460a
lucio albornoz <lucioga32@gmail.com>
2022-07-15 02:27:33 -0300

    ejercicio 2

--- Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
+++ Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
@@ -1,8 +1,44 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("sentencias condicionales e iterativas");
+        String nom_prod = "";
+        double precio = 0, precio_final = 0;
+        int opcion = 0;
+
+        Scanner enter = new Scanner(System.in);
+
+        System.out.println("Ingresa el nombre del producto");
+        nom_prod = enter.nextLine();
+
+        System.out.println("Ingresa el precio: ");
+        precio = enter.nextFloat();
+
+        System.out.println(" op. 1: Contado \n" +
+                " op. 2: Debito \n" +
+                " op. 3: Tarjeta");
+        System.out.println("Ingresa opcion de pago: ");
+        opcion = enter.nextInt();
+
+        switch (opcion) {
+            case 1:
+                precio_final = precio-(precio * 0.15) ;
+                System.out.println("El precio final es: " + precio_final);
+                break;
+            case 2:
+                precio_final = precio;
+                System.out.println("El precio final es: " + precio_final);
+                break;
+            case 3:
+                precio_final = (precio * 0.10) + precio;
+                System.out.println("El precio final es: " + precio_final);
+            default:
+                System.out.println("La opcion es incorrecta");
+        }
+        System.out.println("Tu producto es:" + nom_prod);
+        System.out.println("El precio es:" +precio_final);
     }
 }
