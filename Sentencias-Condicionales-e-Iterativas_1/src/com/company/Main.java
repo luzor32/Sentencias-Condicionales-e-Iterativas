1a2c9b0ec29b4d02e3edba3c0b9d06f3233c5532
lucio albornoz <lucioga32@gmail.com>
2022-07-15 03:29:33 -0300

    metodo ejerciio 8

--- Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
+++ Sentencias-Condicionales-e-Iterativas_1/src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Triangulo3();
+    }
+
+    private static void Triangulo3() {
         String caract="*";
 
         for(int j=0; j<7; j++) {
