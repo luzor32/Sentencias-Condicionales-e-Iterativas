f0d718ab56b9580623a38cebeb3404aab7db5ac0
lucio albornoz <lucioga32@gmail.com>
2024-01-24 17:55:42 -0300

    Ejercicio 1 resuelto

--- Sentencias-Condicionales-e-iterativas/src/Main.java
+++ Sentencias-Condicionales-e-iterativas/src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa un numero entero: ");
+        int num1 = sc.nextInt();
+        System.out.print("Ingresa otro numero entero: ");
+        int num2 = sc.nextInt();
+        if (num1 > num2) {
+            System.out.println("El numero " + num1 + " es mayor que el numero " + num2);
+        } else if (num1 < num2) {
+            System.out.println("El numero " + num2 + " es mayor que el numero " + num1);
+        } else {
+            System.out.println("Los dos numeros son iguales");
+        }
     }
 }
