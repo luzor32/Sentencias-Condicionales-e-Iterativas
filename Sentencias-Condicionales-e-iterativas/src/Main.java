7d08dfcee0087fb2296b5847d7133d2c93367bc9
lucio albornoz <lucioga32@gmail.com>
2024-01-24 19:30:56 -0300

    ejercicio2 resulto

--- Sentencias-Condicionales-e-iterativas/src/Main.java
+++ Sentencias-Condicionales-e-iterativas/src/Main.java
@@ -1,6 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hello world!");
+        System.out.print("Ingrese su producto: ");
+        String producto = sc.nextLine();
+
+        System.out.print("ingrese el precio: ");
+        double precio = sc.nextDouble();
+
+        System.out.println("op 1: Contado  \n"+
+                           "op 2: Debito \n"+
+                           "op 3: Tarjeta" );
+        int opcion = sc.nextInt();
+        double precio_final = 0;
+
+        switch (opcion){
+            case 1:
+                precio_final = precio - (precio * 0.15);
+                break;
+
+            case 2:
+                precio_final = precio;
+                break;
+
+            case 3:
+                precio_final = precio + (precio *0.10);
+                break;
+
+            default:
+                System.out.println("La opcion no es correcta");
+        }
+        System.out.println("---------------------------------------");
+        System.out.println("El nombre del producto es: "+producto);
+        System.out.println("El importe final es: "+precio_final);
+
     }
 }
