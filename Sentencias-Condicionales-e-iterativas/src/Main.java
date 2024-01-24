5821c637b74b7630f7347bce69a2d5286b3ce884
lucio albornoz <lucioga32@gmail.com>
2024-01-24 19:54:34 -0300

    Ejercicio 3 resulto

--- Sentencias-Condicionales-e-iterativas/src/Main.java
+++ Sentencias-Condicionales-e-iterativas/src/Main.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print(" Ingresa el nombre del cursante: ");
+        String nombre = sc.nextLine();
+        System.out.println("Notas del 1 al 10");
+        System.out.print("Ingresa la primera nota: ");
+        double nota1 = sc.nextDouble();
+        System.out.print("Ingresa la segunda nota: ");
+        double nota2 = sc.nextDouble();
+        System.out.print("Ingresa la tercera nota: ");
+        double nota3 = sc.nextDouble();
+
+        double promedio = (nota1 + nota2 + nota3)/3;
+
+        System.out.println("----------------------------------");
+        System.out.println("Nombre del cursante es: "+nombre);
+        System.out.println("Su promedio es: "+promedio);
+
+        if (promedio > 0 && promedio <=5) {
+            System.out.println("El cursante esta desaprobado");
+        } else if (promedio > 5 && promedio <= 8) {
+            System.out.println("El cursante esta aprobado");
+        } else if (promedio > 8 && promedio <= 10) {
+            System.out.println("El cursante esta promocionado");
+        }
     }
 }
