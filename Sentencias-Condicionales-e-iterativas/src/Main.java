e494302b250a47cb2909e45b790871b381be695d
lucio albornoz <lucioga32@gmail.com>
2024-01-24 20:36:53 -0300

    triangulo 2 resuelto

--- Sentencias-Condicionales-e-iterativas/src/Main.java
+++ Sentencias-Condicionales-e-iterativas/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        for (int i = 0; i < 7; i++) {
+            String texto = "";
+            for (int j = 0; j < 7-i; j++) {
+                texto +="*";
+            }
+            System.out.println(texto);
+        }
     }
 }
