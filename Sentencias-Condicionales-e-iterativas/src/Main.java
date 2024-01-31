ae6ae6bd430ea79061aa324ad1c96c5a8b1b1b84
lucio albornoz <lucioga32@gmail.com>
2024-01-31 16:49:07 -0300

    modificacion ex4

--- Sentencias-Condicionales-e-iterativas/src/Main.java
+++ Sentencias-Condicionales-e-iterativas/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        ex4();
+    }
+
+    private static void ex4() {
         for (int i = 0; i < 10; i++) {
             System.out.println("foo");
         }
