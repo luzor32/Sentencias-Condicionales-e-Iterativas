5e0cad87b18550ad1cabebf1a44fbcb38336fd49
lucio albornoz <lucioga32@gmail.com>
2024-01-31 16:52:26 -0300

    modificacíon ex6

--- Sentencias-Condicionales-e-iterativas/src/Main.java
+++ Sentencias-Condicionales-e-iterativas/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        ex6();
+    }
+
+    private static void ex6() {
         for (int i = 0; i < 7; i++) {
             String texto = "";
             for (int j = 0; j < 7-i; j++) {
