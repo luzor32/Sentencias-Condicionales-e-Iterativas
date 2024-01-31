71cdf221ca76a3384f010e3788f583abc9ac7f14
lucio albornoz <lucioga32@gmail.com>
2024-01-31 16:51:04 -0300

    modificacíon ex5

--- Sentencias-Condicionales-e-iterativas/src/Main.java
+++ Sentencias-Condicionales-e-iterativas/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        ex5();
+    }
+
+    private static void ex5() {
         String texto ="";
         for (int i = 0; i < 7; i++) {
             texto +="*";
